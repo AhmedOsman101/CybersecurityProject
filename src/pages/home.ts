a26393f9f04a457f78012963a8ef5946782cc42b
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-22 20:19:38 +0200

    refactor: apply in-line styling
    - Removed water.css CDN dependency
    - Added in-line styling
    - Updated page layout

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,3 +1,5 @@
+import styles from "../styles.ts";
+
 export function render(): string {
   return `
     <!DOCTYPE html>
@@ -6,20 +8,18 @@ export function render(): string {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Cybersecurity Project</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+      <style>${styles}</style>
     </head>
     <body>
-      <center>
-        <div>
+      <header style="text-align: center; padding: 20px;">
           <h1>Encryption/Decryption Tools</h1>
-          <p>Welcome to our homepage!</p>
-          <a href="/sha-1">Use SHA-1</a>
-          <br>
-          <a href="/aes">Use AES with ECB mode</a>
-          <br>
-          <a href="/rsa">Use RSA with LCG key generation</a>
-        </div>
-    </center>
+          <nav>
+            <p>Welcome to our homepage!</p>
+            <a href="/sha-1">Use SHA-1</a><br>
+            <a href="/aes">Use AES with ECB mode</a></br>
+            <a href="/rsa">Use RSA with LCG key generation</a>
+          </nav>
+        </header> 
     </body>
     </html>`;
 }
