f710627aa459702725674d95f05dbd64f6195476
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-23 19:47:41 +0200

    refactor(sha1): use components for layout
    - Use Navbar component
    - Use Body component
    - Simplify render function

--- src/pages/sha1.ts
+++ src/pages/sha1.ts
@@ -1,63 +1,56 @@
-import styles from "../styles.ts";
+import Body from "./components/body.ts";
+import Navbar from "./components/navbar.ts";
 
 type Props = {
   text: string;
   result: string;
 };
 
-export function render(props: Props): string {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0" />
-        <title>Cybersecurity Project - SHA-1</title>
-        <style>${styles}</style>
+export function render({ text, result }: Props): string {
+  const links = [
+    {
+      url: "/",
+      label: "Homepage",
+    },
+    {
+      url: "/aes",
+      label: "Use AES with ECB mode",
+    },
+    {
+      url: "/rsa",
+      label: "Use RSA with LCG key generation",
+    },
+  ];
 
-      </head>
-      <body>
-        <header style="text-align: center; padding: 20px;">
-          <h1>Encryption Using SHA-1</h1>
-          <nav>
-            <p>Navigation</p>
-            <a href="/">Homepage</a><br>
-            <a href="/aes">Use AES with ECB mode</a></br>
-            <a href="/rsa">Use RSA with LCG key generation</a>
-          </nav>
-        </header>
+  const slot = `
+  ${Navbar("Encryption Using SHA-1", links)}
+  <main class="sha-container">
+    <section class="panel">
+      <h1>SHA-1 Hash</h1>
+      <form action="/sha-1" method="post">
+        <label for="text">Text to hash:</label>
+        <input
+          type="text"
+          id="text"
+          name="text"
+          placeholder="Enter text to hash"
+          value="${text}" 
+          required
+        />
 
-        <center>
-          <div>
-            <h1>SHA-1</h1>
-            <form
-              action="/sha-1"
-              method="post">
-              <label for="text">Text: </label>
-              <input
-                placeholder="Enter text to hash"
-                type="text"
-                id="text"
-                name="text"
-                value="${props.text}"
-                required />
-              <br />
-              <label for="result">Result:</label>
-              <textarea
-                type="text"
-                id="result"
-                rows="2"
-                name="result"
-                readonly>${props.result}</textarea>
-              <br />
-              <button type="submit">Encrypt</button>
-            </form>
-            <br />
-          </div>
-        </center>
-      </body>
-    </html>
+        <label for="result">Hash Result:</label>
+        <textarea
+          id="result"
+          name="result"
+          rows="2"
+          readonly
+        >${result}</textarea>
+
+        <button type="submit">Encrypt</button>
+      </form>
+    </section>
+  </main>
   `;
+
+  return Body("Cybersecurity Project - SHA-1", slot);
 }
