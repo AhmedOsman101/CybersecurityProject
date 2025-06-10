794325c1ebd4f4e5261f8f6942140f43707be303
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-06-10 17:31:28 +0300

    refactor(lcg): use readonly and remove const
    - Use readonly for constants
    - Remove redundant 'as const'

--- src/services/lcgService.ts
+++ src/services/lcgService.ts
@@ -1,17 +1,21 @@
-/*
-These values are common parameters for LCG.
-They are based on the parameters table provided from wikipedia (MMIX by Donald Knuth):
-https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
-*/
-
 export class Lcg {
-  private static MODULUS = 18_446_744_073_709_551_616n as const; // m => A prime number (2^16+1)
-  private static MULTIPLIER = 6_364_136_223_846_793_005n as const; // a => Primitive root modulo
+  /*
+    These values are common parameters for LCG.
+    They are based on the parameters table provided from wikipedia (MMIX by Donald Knuth):
+    https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+  */
+
+  // m => A power of 2 (2^64) for efficient 64-bit arithmetic
+  private static readonly MODULUS = 18_446_744_073_709_551_616n;
+
+  // a => Primitive root modulo
+  private static readonly MULTIPLIER = 6_364_136_223_846_793_005n as const;
 
   // c => Setting c = 0 makes this a Multiplicative Congruential Generator (MCG)
-  private static INCREMENT = 1_442_695_040_888_963_407n as const;
+  private static readonly INCREMENT = 1_442_695_040_888_963_407n as const;
 
-  private static INITIAL_SEED = BigInt(Date.now()); // Random initial seed within the range [1, m - 1]
+  // Random initial seed within the range [1, m - 1]
+  private static readonly INITIAL_SEED = BigInt(Date.now());
 
   private static *lcgGenerator(
     m: bigint,
